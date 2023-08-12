d542052e79919c0d21429cc06b551cea41610fb7
“KseGreb” <ksegreb@gmail.com>
2023-08-12 11:22:22 -0600

    css

--- src/myRecipeComponents.js
+++ src/myRecipeComponents.js
@@ -1,19 +1,20 @@
-// import { useState } from "react";
-// import { Redirect } from 'react-router-dom';
-
-
-function MyRecipesComponents({label, calories, image, ingredients, direction, url}){
+function MyRecipesComponents({label, calories, image, ingredients, url}){
 
     return(
         <div>
             <div className="container">
                 <h2>{label}</h2>
             </div>
+
             <div className="container">
-            <a href={url}><img src={image} alt="food"/></a>
+                <a href={url}><img src={image} alt="food"/></a>
             </div>
             <div className="container">
-                <ul className="list">
+                <h3>{calories.toFixed()} calories</h3>
+                {/* <h3>{Math.round(calories)} calories</h3> */}
+            </div>
+            
+                <ul className="container list">
                     {ingredients.map((ingredient, index) => (
                         <li key={index}> 
                             <img className="icon" src="https://img.icons8.com/external-kiranshastry-gradient-kiranshastry/64/000000/external-check-multimedia-kiranshastry-gradient-kiranshastry.png " alt="icon"/>
@@ -21,11 +22,8 @@ function MyRecipesComponents({label, calories, image, ingredients, direction, ur
                         </li>
                     ))}
                 </ul>
-            </div>
-            <div className="container">
-                <h3>{calories.toFixed()} calories</h3>
-                {/* <h3>{Math.round(calories)} calories</h3> */}
-            </div>
+            
+
         </div>
     )
 }
